194673fb65d171f92fbafdd9dfdc220f21bde5de
AdrienAllouis <adrien.alls@protonmail.com>
2022-05-06 10:47:14 +0200

    setup image position into cells

--- src/components/OneCell.js
+++ src/components/OneCell.js
@@ -1,13 +1,30 @@
 import styled from 'styled-components'
 
-const Cell = styled.img`
-  background-color: red;
+const CellContainer = styled.div`
+  background-color: cyan;
   aspect-ratio: 1;
+  display: grid;
+  place-items: center;
+
+  ::before {
+    content: "";
+    padding-bottom: 100%;
+    display: block;
+    grid-area: 1 / 1 / 2 / 2;
+  }
 `
 
-const OneCell = () => {
+const Cell = styled.img`
+  max-width: 100%;
+  max-heigth: 100%;
+  grid-area: 1 / 1 / 2 / 2;
+`
+
+const OneCell = ({ id }) => {
   return (
-    <Cell src="" alt="" />
+    <CellContainer>
+      <Cell src={`https://picsum.photos/id/${id}/200`} alt="" />
+    </CellContainer>
   )
 }
 
