c51d06d0d41664e97a3bdab039c7d46b00b6f669
vleborgne <vleborgne@gmail.com>
2022-05-17 10:05:38 +0200

    State loading

--- src/store/mapInfoSlice.js
+++ src/store/mapInfoSlice.js
@@ -3,6 +3,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import {TOTAL_CELLS} from '../index'
 
 const initialState = {
+    loading: false,
     imgMap: [],
 };
 
@@ -32,6 +33,7 @@ export const mapInfoSlice = createSlice({
         builder.addCase(getAllMapInfo.fulfilled, (state, action) => {
             let imageTable = action.payload;
             let newImageArray = [];
+            // Complete the array with empty cells 
             if(imageTable.length < TOTAL_CELLS){ 
                 const emptyArray = Array.apply(null, Array(TOTAL_CELLS-imageTable[0].length)).map( ()=>({}) )
                 newImageArray = [...imageTable[0], ...emptyArray];
@@ -39,10 +41,16 @@ export const mapInfoSlice = createSlice({
                 newImageArray = imageTable[0];
             }
             state.imgMap = [newImageArray,imageTable[1] ];
+            state.loading = false;
 
+        })
+        builder.addCase(getAllMapInfo.pending, (state, action) => {
+           state.loading = true;
         })
     },
 });
 export const mapSelector = (state) => state.map.imgMap;
+export const isMapLoading = (state) => state.map.loading;
+
 
 export const { mockData, nftImgMap } = mapInfoSlice.actions;
