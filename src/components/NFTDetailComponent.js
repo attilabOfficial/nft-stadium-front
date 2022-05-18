19ad1d85b3737471c073f9c58d035fd5814792af
vleborgne <vleborgne@gmail.com>
2022-05-18 07:09:21 +0200

    Don't display img detail if null

--- src/components/NFTDetailComponent.js
+++ src/components/NFTDetailComponent.js
@@ -18,14 +18,12 @@ export const NFTDetailComponent = ({ currentNFT, changeImgFct }) => {
       <h2>#{currentNFT.id}</h2>
       <h3>Owner :</h3>
       <p>{currentNFT.owner}</p>
-      <img src={currentNFT.img} alt='NFT' />
+      {currentNFT.img !== "" &&  <img src={currentNFT.img} alt='NFT' />}
 
       <h5>Change Image</h5>
       <input type="url" value={imgUrl} onChange={changeImg}/>
       <button onClick={()=>{
-        console.log(changeImgFct)
         changeImgFct(imgUrl)
-        console.log("ok")
 
       }}>Go! </button>
 
